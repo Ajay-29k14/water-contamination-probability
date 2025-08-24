58656e23492de0e4c43e9bad0f37e041680fdf83
Ajay <ajay.p.14.kumar@gmail.com>
2025-08-24 15:25:02 +0530

    requirement change

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ COLUMNS = bundle["cols"]  # ["rainfall_3h","temperature","humidity","turbidity"]
 app = FastAPI(title="Contamination Risk API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Vite dev URLs
+    allow_origins=["*"],  # Vite dev URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
